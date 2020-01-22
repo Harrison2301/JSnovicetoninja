26669bbab646f5bacadb3956dcd09afa1018dee4
Harrison2301 <harrisonwinters2301@gmail.com>
2020-01-22 14:50:04 +0000

    different variable types

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,1 +1,14 @@
-alert('hello,world');
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30; 
+console.log(age);
+
+const points = 100; 
+// const points = 50;
+console.log(points);
+
+var score = 75; 
+console.log(score);
