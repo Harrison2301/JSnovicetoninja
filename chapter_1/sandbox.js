01efea0e886e0be8e3bd69996580683e1730eff7
Harrison2301 <harrisonwinters2301@gmail.com>
2020-01-29 14:06:21 +0000

    sandbox clean up - more variable methods

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,40 +1,13 @@
-// let age = 25;
-// let year = 2019;
 
-// console.log(age, year);
+let email = 'harrison@hotmail.com';
 
-// age = 30; 
-// console.log(age);
+// let result = email.indexOf('@');
 
-// const points = 100; 
-// // const points = 50;
-// console.log(points);
+// let result = email.lastIndexOf('n');
 
-// var score = 75; 
-// console.log(score);
+// let result = email.slice(0,5);
 
-// strings concatenation
+// let result = email.substr(4, 10)
 
-let firstName = 'Harrison';
-let lastName = 'Winters';
-
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
-
-//getting Characters 
-console.log(fullName[2]);
-
-// string length - property
-console.log(fullName.length);
-
-// string - uppercase - method
-
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
+let result = email.replace('n', 'w')
 console.log(result);
-
-let email = "test@hotmail.com";
-console.log(email);
-
-let index = email.indexOf('@');
-console.log(index);
