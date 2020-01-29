036a7522b765ed98969c09c252e9b56473e8a39e
Harrison2301 <harrisonwinters2301@gmail.com>
2020-01-29 13:59:54 +0000

    update variable in ES6 and string properties

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,14 +1,40 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
-console.log(age, year);
+// console.log(age, year);
 
-age = 30; 
-console.log(age);
+// age = 30; 
+// console.log(age);
 
-const points = 100; 
-// const points = 50;
-console.log(points);
+// const points = 100; 
+// // const points = 50;
+// console.log(points);
 
-var score = 75; 
-console.log(score);
+// var score = 75; 
+// console.log(score);
+
+// strings concatenation
+
+let firstName = 'Harrison';
+let lastName = 'Winters';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//getting Characters 
+console.log(fullName[2]);
+
+// string length - property
+console.log(fullName.length);
+
+// string - uppercase - method
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+let email = "test@hotmail.com";
+console.log(email);
+
+let index = email.indexOf('@');
+console.log(index);
