e94fa5565852402ca2d5f684c5504bb153af9331
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-04 02:33:52 +0000

    mix data types in an array

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,4 +1,7 @@
 let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-ninjas[1] = 'ken';
-console.log(ninjas)
+// ninjas[1] = 'ken';
+// console.log(ninjas)
+
+let random = ['shaun', 'crystal', 30, 20];
+console.log(random);
