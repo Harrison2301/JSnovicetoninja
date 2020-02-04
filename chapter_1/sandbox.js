4baf71de8b0854b8e806713d1f2fbcaac8e6d981
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-04 02:21:10 +0000

    created ninjas array, overode

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,19 +1,4 @@
-//template strings 
+let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-const title = 'Best reads of 2019';
-const author = 'Mario';
-const likes = 30; 
-
-//concatenation way
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
-// console.log(result)
-
-//template string way
-let result = `The blog called ${title} by ${author} has ${likes} likes`
-console.log(result)
-
-//creating html templates
-let html = `
-<h2>${title}</h2>
-<p>By ${author}</p>
-<span>This blog has ${likes} like</span>`
+ninjas[1] = 'ken';
+console.log(ninjas)
