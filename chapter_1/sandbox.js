1c32971772ec586fd798189fcfb7d6968ca8647b
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-05 01:24:18 +0000

    expored type conversions

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,12 +1,37 @@
-let age = 23;
+// let age = 23;
 
-// loose comparison (different types can still be equal)
+// // loose comparison (different types can still be equal)
 
-// console.log(age == 23);
-// console.log(age =='23');
+// // console.log(age == 23);
+// // console.log(age =='23');
 
-// strict comparison (different types cannot be equal)
+// // strict comparison (different types cannot be equal)
 
-console.log(age === 23)
-console.log(age === '23')
-console.log(age !== '23')
+// console.log(age === 23)
+// console.log(age === '23')
+// console.log(age !== '23')
+
+//  type conversion
+
+let score = '100';
+
+// score = Number(score);
+
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = Number('hello')
+// console.log(result)
+
+// let result = String(50);
+
+// console.log(result, typeof result);
+
+
+
+// let result = Boolean(100);
+let result = Boolean('0');
+
+
+console.log(result, typeof result);
+
