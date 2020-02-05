85ecb8e6e9c83260f3ed4c5f83865590743d8aee
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-05 01:13:19 +0000

    loose comparison booleans

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,3 +1,28 @@
-let age = null;
+// booleans & comparisons
 
-console.log(age, age + 3, `the age is ${age}`);
+//methods can return booleans
+let email = 'harrison@email.com'
+let names = ['mario', 'luigi', 'toad'];
+
+// let result = email.includes('!')
+// let result = names.includes('harrison')
+
+// console.log(result);
+
+//comparison operators
+
+let age = 23;
+
+// console.log(age == 23);
+// console.log(age == 32);
+// console.log(age != 32);
+// console.log(age > 32);
+// console.log(age < 32);
+// console.log(age <= 32);
+// console.log(age >= 23);
+
+let name = 'harrison';
+
+console.log(name == 'harrison')
+console.log(name == 'Harrison')
+
