b7ee6797c6163d3452ab6285c64c828ff24ec65b
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-07 02:03:16 +0000

    review while loop example

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -7,8 +7,18 @@
 
 const names = ['harrison', 'mario', 'luigi'];
 
-for(let i = 0; i < names.length; i++){
-    // console.log(names[i]);
-    let html = `<div>${names[i]}</div>`
-    console.log(html);
+// for(let i = 0; i < names.length; i++){
+//     // console.log(names[i]);
+//     let html = `<div>${names[i]}</div>`
+//     console.log(html);
+// }
+
+// while loops
+
+let i = 0;
+
+while(i < names.length){
+console.log(names[i]);
+i++;
 }
+
