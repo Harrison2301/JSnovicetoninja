87decee8a0430b3709dce956dd2a320384488d69
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-10 02:04:34 +0000

    if statement review

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,32 +1,18 @@
-// for loops 
+// // if statements
+// const age = 25;
 
-// for(let i = 0; i < 5; i++){
-//     console.log('in loop:', i);
-// }
-// console.log('loop finished');
-
-const names = ['harrison', 'mario', 'luigi'];
-
-// for(let i = 0; i < names.length; i++){
-//     // console.log(names[i]);
-//     let html = `<div>${names[i]}</div>`
-//     console.log(html);
+// if(age > 20){
+//     console.log('you are over 20 years old')
 // }
 
-// while loops
+// const ninjas = ['harrison', 'ryu', 'chun-li', 'yoshi']
 
-// let i = 0;
-
-// while(i < names.length){
-// console.log(names[i]);
-// i++;
+// if(ninjas.length > 4){
+//     console.log("that is a lot of ninjas")
 // }
 
-// do while loops 
+const password = 'pass';
 
-let i = 0;
-
-do{
-    console.log('val of i is ', i);
-    i++;
-}while(i < 5);
+if(password.length >= 8){
+    console.log('that password is long enough')
+}
