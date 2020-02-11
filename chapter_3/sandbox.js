f1658376b8896b9b8fac7003e89a64f585f12986
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-11 23:35:21 +0000

    took a look at switch statements

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -33,18 +33,42 @@
 
 // break and continue 
 
-const scores = [50, 25, 0, 30, 100, 20, 10];
+// const scores = [50, 25, 0, 30, 100, 20, 10];
 
-for(let i = 0; i < scores.length; i++){
+// for(let i = 0; i < scores.length; i++){
 
-    if(scores[i] === 0){
-        continue;
-    }
+//     if(scores[i] === 0){
+//         continue;
+//     }
 
-    console.log('your score: ', scores[i]);
+//     console.log('your score: ', scores[i]);
 
-    if(scores[i] === 100){
-        console.log('congrats, top score!')
+//     if(scores[i] === 100){
+//         console.log('congrats, top score!')
+//         break;
+//     }
+// }
+
+// switch statements
+
+const grade = 'A';
+
+switch(grade){
+    case 'A':
+        console.log('You got an A');
         break;
-    }
+        case 'A':
+                console.log('You got an A');
+        break;
+        case 'B':
+                console.log('You got an B');
+        break;
+        case 'C':
+                console.log('You got an C');
+        break;
+        case 'D':
+                console.log('You got an D');
+        break;
+        default:
+                console.log('not a valid grade');
 }
