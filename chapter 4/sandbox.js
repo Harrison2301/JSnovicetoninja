aeb23eb76ed23eb283ea00ce1cf9adcc58f04c86
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-12 00:10:55 +0000

    review functions

---
+++ chapter 4/sandbox.js
@@ -0,0 +1,47 @@
+// //function expression -not hoisted
+
+// const speak = function(){
+// console.log('good day')
+// };
+
+// greet();
+
+// speak();
+
+// //function declaration -hoisted
+// function greet(){
+//     console.log('hello there');
+// }
+
+//arguments & parameters
+
+// //parameters
+// const speak = function(name = 'no-named fool', time = 'wheres the time silly'){
+//     console.log(`good ${time} ${name}`);
+// };
+
+
+//  //argument
+// speak('harrison', 'morning');
+// speak();
+
+// returning values 
+
+// const calcArea = function(radius){
+//     let area = 3.14 * radius**2;
+//     return area;
+// }
+
+// const area = calcArea(5);
+
+// console.log(area)
+
+
+ // arrow functions
+
+ const calcArea = radius => 3.14 * radius**2;
+
+  const area = calcArea(5);
+
+console.log(area)
+ 
