740d17a8cee2c66342fca134acaa34896533263c
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-27 01:53:55 +0000

    create an html template for each person in an array

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -12,19 +12,44 @@
 // console.log(errors);
 
 //element by id
-const title = document.getElementById('page-title');
-console.log(title)
+// const title = document.getElementById('page-title');
+// console.log(title)
 
-//element by class name
+// //element by class name
 
-const errors = document.getElementsByClassName('error');
-console.log(errors)
-console.log(errors[0])
+// const errors = document.getElementsByClassName('error');
+// console.log(errors)
+// console.log(errors[0])
 
-//get element by tag name
+// //get element by tag name
 
-const paras = document.getElementsByTagName('p');
-console.log(paras)
-console.log(paras[0]);
+// const paras = document.getElementsByTagName('p');
+// console.log(paras)
+// console.log(paras[0]);
 
 // using queryselector or selector all is great because its simple and you are able to use for each 
+
+// const para = document.querySelector('p')
+
+// para.innerText += ', this has been updated';
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(para => {
+    para.innerText += ', this has been updated'
+})
+
+const content = document.querySelector('.content');
+
+content.innerHTML = '<h2> this is a new H2!!!! </h2>'
+
+
+
+// *************************
+const people = [
+    'mario', 'luigi', 'harrison'
+]
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`
+})
