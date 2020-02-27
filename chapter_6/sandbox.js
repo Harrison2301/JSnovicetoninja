deef404c44538aa4ec45e28d7606f2256f9a6187
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-27 01:38:37 +0000

    other ways to do querys

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,12 +1,30 @@
-//selector tip -> you can right click and select inspect, in elements tab you can right click to copy selector 
-// const para = document.querySelector('div.error');
+// //selector tip -> you can right click and select inspect, in elements tab you can right click to copy selector 
+// // const para = document.querySelector('div.error');
 
-// console.log(para);
+// // console.log(para);
 
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 
-// paras.forEach(para => {
-//     console.log(para);
-// })
-const errors = document.querySelectorAll('.error')
-console.log(errors);
+// // paras.forEach(para => {
+// //     console.log(para);
+// // })
+// const errors = document.querySelectorAll('.error')
+// console.log(errors);
+
+//element by id
+const title = document.getElementById('page-title');
+console.log(title)
+
+//element by class name
+
+const errors = document.getElementsByClassName('error');
+console.log(errors)
+console.log(errors[0])
+
+//get element by tag name
+
+const paras = document.getElementsByTagName('p');
+console.log(paras)
+console.log(paras[0]);
+
+// using queryselector or selector all is great because its simple and you are able to use for each 
