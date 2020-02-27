514779444c54f0fb2d11c04140295dcabdad273c
Harrison2301 <harrisonwinters2301@gmail.com>
2020-02-27 01:30:39 +0000

    look at query selectors using the dom

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,12 @@
+//selector tip -> you can right click and select inspect, in elements tab you can right click to copy selector 
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para);
+// })
+const errors = document.querySelectorAll('.error')
+console.log(errors);
