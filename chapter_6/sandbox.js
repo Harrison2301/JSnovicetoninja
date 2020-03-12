16366268c5844c1e1596fb1f1a4a9849bd24e380
Harrison2301 <harrisonwinters2301@gmail.com>
2020-03-12 01:01:22 +0000

    play around node relationship methods

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -96,8 +96,13 @@
 const article = document.querySelector('article');
 
 
-Array.from(article.children).forEach(child => {
-child.classList.add('article-element');
-});
+// Array.from(article.children).forEach(child => {
+// child.classList.add('article-element');
+// });
 
+const title = document.querySelector('h2');
 
+console.log(title.parentElement)
+console.log(title.parentElement.parentElement)
+console.log(title.nextElementSibling)
+console.log(title.previousElementSibling)
