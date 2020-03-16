8f49100183af5499b0733126fbdaae0112025f80
Harrison2301 <harrisonwinters2301@gmail.com>
2020-03-16 00:11:23 +0000

    add event listener to todo list

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -107,8 +107,19 @@
 // console.log(title.nextElementSibling)
 // console.log(title.previousElementSibling)
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
-    console.log('you clicked me')
+// button.addEventListener('click', () => {
+//     console.log('you clicked me')
+// })
+
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+    item.addEventListener('click', e => {
+        // console.log('item clicked')
+        // console.log(e)
+        // console.log(e.target)
+        e.target.style.textDecoration = 'line-through'
+    }) 
 })
