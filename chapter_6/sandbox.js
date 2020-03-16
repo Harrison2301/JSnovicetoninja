e7d2ae8884a2b76b17a9a3428f66f4d0744253c9
Harrison2301 <harrisonwinters2301@gmail.com>
2020-03-16 00:02:49 +0000

    simple to do set up

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -93,16 +93,16 @@
 //        p.classList.add('success')
 //    }
 // });
-const article = document.querySelector('article');
+// const article = document.querySelector('article');
 
 
-// Array.from(article.children).forEach(child => {
-// child.classList.add('article-element');
-// });
+// // Array.from(article.children).forEach(child => {
+// // child.classList.add('article-element');
+// // });
 
-const title = document.querySelector('h2');
+// const title = document.querySelector('h2');
 
-console.log(title.parentElement)
-console.log(title.parentElement.parentElement)
-console.log(title.nextElementSibling)
+// console.log(title.parentElement)
+// console.log(title.parentElement.parentElement)
+// console.log(title.nextElementSibling)
 // console.log(title.previousElementSibling)
