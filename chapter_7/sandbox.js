e73457320dd81c80b4a69cad6ec32f3706be4a52
Harrison2301 <harrisonwinters2301@gmail.com>
2020-03-30 19:54:08 +0000

    basic form validation

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,25 +1,37 @@
 const form = document.querySelector('.signup-form');
 // const username = document.querySelector('#username')
+const feedback = document.querySelector('.feedback')
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    // console.log(username.value)
-    console.log(form.username.value);
+   
+    // validation
+  const username = form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+  if(usernamePattern.test(username)){
+  //feedback good info
+  feedback.textContent = "that username is valid!";
+  } else {
+      //feedback bad info
+      feedback.textContent = "username not valid";
+  }
+
 });
 
-const username = 'harrison909090';
-const pattern = /^[a-z]{6,}$/
+// const username = 'harrison909090';
+// const pattern = /^[a-z]{6,}$/
 
-// let result = pattern.test(username);
+// // let result = pattern.test(username);
 
 
 
-// if(result){
-//     console.log('regex test passed')
-// } else {
-//     console.log('regex test failed')
-// }
+// // if(result){
+// //     console.log('regex test passed')
+// // } else {
+// //     console.log('regex test failed')
+// // }
 
-let result = username.search(pattern);
+// let result = username.search(pattern);
 
-console.log(result);
+// console.log(result);
