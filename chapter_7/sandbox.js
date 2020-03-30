7330f0292583704aca6c5e8540a7885ecf4adfec
Harrison2301 <harrisonwinters2301@gmail.com>
2020-03-30 19:47:37 +0000

    test RegEx patterns

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -6,3 +6,20 @@ form.addEventListener('submit', e => {
     // console.log(username.value)
     console.log(form.username.value);
 });
+
+const username = 'harrison909090';
+const pattern = /^[a-z]{6,}$/
+
+// let result = pattern.test(username);
+
+
+
+// if(result){
+//     console.log('regex test passed')
+// } else {
+//     console.log('regex test failed')
+// }
+
+let result = username.search(pattern);
+
+console.log(result);
