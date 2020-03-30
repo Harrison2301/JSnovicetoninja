ed2482ec0f9dbb78e731ec149ce0592dac4c4f5c
Harrison2301 <harrisonwinters2301@gmail.com>
2020-03-30 20:07:30 +0000

    add live feedback to username

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -2,12 +2,14 @@ const form = document.querySelector('.signup-form');
 // const username = document.querySelector('#username')
 const feedback = document.querySelector('.feedback')
 
+const username = form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
 form.addEventListener('submit', e => {
     e.preventDefault();
    
     // validation
-  const username = form.username.value;
-  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+  
 
   if(usernamePattern.test(username)){
   //feedback good info
@@ -18,6 +20,16 @@ form.addEventListener('submit', e => {
   }
 
 });
+
+//live feedback
+
+form.username.addEventListener('keyup', e => {
+  if(usernamePattern.test(e.target.value)){
+    form.username.setAttribute('class','success');
+  } else {
+    form.username.setAttribute('class','error');
+  }
+})
 
 // const username = 'harrison909090';
 // const pattern = /^[a-z]{6,}$/
