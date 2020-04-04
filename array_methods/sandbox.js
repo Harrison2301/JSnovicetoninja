1048d8ab029368eec77a613ba4c4415337feedba
Harrison2301 <harrisonwinters2301@gmail.com>
2020-04-04 17:52:10 +0000

    array map method

--- array_methods/sandbox.js
+++ array_methods/sandbox.js
@@ -1,4 +1,31 @@
-const scores = [10, 30, 15, 25, 50, 50, 5];
+const prices = [10, 30, 15, 25, 50, 50, 5];
+
+// ****** Map
+// const salePrices = prices.map((price) => {
+// return price / 2;
+// });
+
+// console.log(salePrices);
+
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50},
+]
+
+const filteredSales = products.map((product) => {
+    if(product.price > 30) {
+        return {name: product.name, price: product.price/2}
+    } else {
+        return product;
+    }
+})
+
+console.log(filteredSales)
+
+// ******* Filter
 
 // const filteredScores =scores.filter((score) => {
 //     return score > 20
@@ -6,16 +33,16 @@ const scores = [10, 30, 15, 25, 50, 50, 5];
 
 // console.log(filteredScores)
 
-users = [
-    {name: 'Harrison', premium: true},
-    {name: 'Jack', premium: false},
-    {name: 'Tim', premium: true},
-    {name: 'James', premium: false},
-    {name: 'Joe', premium: false},
-];
+// users = [
+//     {name: 'Harrison', premium: true},
+//     {name: 'Jack', premium: false},
+//     {name: 'Tim', premium: true},
+//     {name: 'James', premium: false},
+//     {name: 'Joe', premium: false},
+// ];
 
-const premiumUsers = users.filter((user) => {
-return user.premium;
-});
+// const premiumUsers = users.filter((user) => {
+// return user.premium;
+// });
 
-console.log(premiumUsers);
+// console.log(premiumUsers);
