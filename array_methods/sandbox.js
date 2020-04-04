280e7d95eeb590795ae5bd4d5cdde89683931c42
Harrison2301 <harrisonwinters2301@gmail.com>
2020-04-04 17:36:52 +0000

    filter array method

--- array_methods/sandbox.js
+++ array_methods/sandbox.js
@@ -1,5 +1,21 @@
 const scores = [10, 30, 15, 25, 50, 50, 5];
 
-scores.filter((score) => {
-    return score > 20
-})
+// const filteredScores =scores.filter((score) => {
+//     return score > 20
+// })
+
+// console.log(filteredScores)
+
+users = [
+    {name: 'Harrison', premium: true},
+    {name: 'Jack', premium: false},
+    {name: 'Tim', premium: true},
+    {name: 'James', premium: false},
+    {name: 'Joe', premium: false},
+];
+
+const premiumUsers = users.filter((user) => {
+return user.premium;
+});
+
+console.log(premiumUsers);
